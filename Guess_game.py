82115a756e00d62781725a0fdc9b7e59f4bc5fd6
Prince Doobay <107308736+princedoobay@users.noreply.github.com>
2022-06-25 18:58:17 +0530

    Update Guess_game.py

--- Guess_game.py
+++ Guess_game.py
@@ -1,13 +1,13 @@
 import random
-jackpot = random.randint(1,100)
-guess = int(input("chal guess kar: "))
+jackpot = random.randint(1,500)
+guess = int(input("Guess a number: "))
 counter = 1
 while guess != jackpot:
     if guess < jackpot:
         print("Guess higher")
     else :
         print("Guess lower")
-    guess = int(input("chal guess kar: "))
+    guess = int(input("Guess a number: "))
     counter += 1
-print("sahi jawab")
+print("Correct Answer")
 print("you took", counter, "attempts")
